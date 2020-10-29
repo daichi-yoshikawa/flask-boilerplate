a86aa41b2dc66c1c91d0fcc5aa1e4d05dc8df1cb
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2020-10-29 10:47:16 -0700

    Modified pytest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,46 @@
 import os
-import sys
-sys.path.append('../')
 
 import pytest
 
-from app.models import db
+from app import create_app
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def init_env():
   os.environ['FLASK_ENV'] = 'testing'
   os.environ['FLASK_APP'] = 'app'
 
 
+@pytest.fixture(scope="session")
+def app(init_env):
+  app = create_app()
+  return app
+
+
+@pytest.fixture(scope="session")
+def client(app):
+  assert app.config['ENV'] == 'testing'
+  return app.test_client()
+
+
+@pytest.fixture(scope="session")
+def db(app):
+  assert app.config['ENV'] == 'testing'
+  assert 'test' in app.config['SQLALCHEMY_DATABASE_URI']
+
+  from app.models import db
+
+  with app.app_context():
+    db.create_all()
+    yield db
+    db.drop_all()
+
+
+@pytest.fixture(scope="session")
+def base_url():
+  return 'http://localhost'
+
+
 @pytest.fixture()
-def init_db():
-  pass
+def headers():
+  return {'Content-Type': 'application/json'}
