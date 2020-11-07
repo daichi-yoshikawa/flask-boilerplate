9c6130455b95b87ca7b2ca503e29e3598762dbc2
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2020-11-06 20:01:52 -0800

    Implemented catch_all for single page application. Also, changed default static dir.

--- app/views/index.py
+++ app/views/index.py
@@ -2,6 +2,12 @@ from flask import render_template
 from . import index_blueprint as index
 
 
-@index.route('/', methods=['GET'])
-def index():
-  return render_template('index.html')
+@index.route('/', defaults={'path': ''})
+@index.route('/<path:path>')
+def catch_all(path):
+  try:
+    return render_template('index.html')
+  except TemplateNotFound:
+    abort(404)
+
+
